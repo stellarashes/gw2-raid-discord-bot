012e043439220f895bb0874941d84c3054eebe6d
stellarashes <xmc3ify@gmail.com>
2018-05-02 22:04:38 -0400

    Note on a required manual modification to make this work

--- package.bat
+++ package.bat
@@ -1,4 +1,8 @@
 call npm run compile
+
+# requires modifying ffmpeg-binaries's index to export a different ffmpeg path
+# module.exports.ffmpegPath = () => path.join(process.cwd(), 'ffmpeg');
+
 call pkg --targets node9-win-x64 package.json -o release\gw2-raid-discord-bot.exe
 call copy node_modules\ffmpeg-binaries\bin\ffmpeg.exe release\
 call copy node_modules\node-opus\build\Release\*.node release\
